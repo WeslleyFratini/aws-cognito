3e9ef155bbbe5038bd82871f8f1413a9f81195ef
WeslleyFratini <weeslleey354@gmail.com>
2021-04-28 21:58:03 -0300

    adicionando amplify config

---
+++ src/amplify-config.js
@@ -0,0 +1,8 @@
+
+const amplifyConfig = {
+    "aws_cognito_region": "REGIAO_DO_COGNITO",
+    "aws_user_pools_id": "USER_POOL_ID",
+    "aws_user_pools_web_client_id": "USER_POOL_CLIENT_ID"
+};
+
+export default amplifyConfig;
