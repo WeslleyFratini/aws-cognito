dd083bff54aed98bb09471a3bb0f601d864221e9
WeslleyFratini <weeslleey354@gmail.com>
2021-04-29 19:48:45 -0300

    feat: create form sign in cognito

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Amplify from 'aws-amplify';
 import amplify_config from './amplify-config';
 
+import SignUpForm from './CognitoReactSignUpForm'
+import SignInForm from './CognitoReactSignInForm';
+
 Amplify.configure(amplify_config);
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My React Cognito App</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    signUpIsActive: false
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.toggleActivePage = this.toggleActivePage.bind(this);
+  }
+
+  toggleActivePage() {
+    this.setState({ signUpIsActive: !this.state.signUpIsActive });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My React Cognito App</h1>
+        <button className="btn btn-light btn-toggle" onClick={this.toggleActivePage}>
+          { this.state.signUpIsActive ? "Ir para login" : "Ir para registro" }
+        </button>
+        { this.state.signUpIsActive ? <SignUpForm /> : <SignInForm /> }
+      </div>
+    );
+  }
+  
 }
 
 export default App;
--- src/amplify-config.js
+++ src/amplify-config.js
@@ -1,8 +1,9 @@
 
 const amplifyConfig = {
-    "aws_cognito_region": "REGIAO_DO_COGNITO",
-    "aws_user_pools_id": "USER_POOL_ID",
-    "aws_user_pools_web_client_id": "USER_POOL_CLIENT_ID"
+    "aws_project_region": "MY_PROJECT_REGION",
+    "aws_cognito_region": "MY_USER_POOL_REGION",
+    "aws_user_pools_id": "MY_USER_POOL_ID",
+    "aws_user_pools_web_client_id": "MY_USER_POOL_WEB_CLIENT_ID"
 };
 
 export default amplifyConfig;
